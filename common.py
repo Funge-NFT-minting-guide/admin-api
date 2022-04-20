dba9906f2f812a2d5e89df1d4db81cccd3d24b4e
vulpes0zedra <vulpes0zedra@gmail.com>
2022-04-21 06:24:07 +0900

    seperate from admin-web

---
+++ common.py
@@ -0,0 +1,9 @@
+SERVICE_HOST = '0.0.0.0'
+SERVICE_URL = 'http://127.0.0.1'
+SERVICE_PORT = 9000
+
+UPLOAD_PATH='uploads'
+
+SUC_CREATED = ('Created.', 201)
+SUC_DELETED = ('Resource deleted successfully.', 200)
+ERR_NOT_FOUND = (404, 'No result found.')
